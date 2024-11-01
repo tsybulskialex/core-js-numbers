456b6260e8d2af613ea4231d7f03b9de1289f652
Aleksandr <tsybulskialex@gmail.com>
2024-11-01 15:30:54 +0300

    feat: 33 tasks completed

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -255,7 +255,7 @@ function toNumber(value, def) {
   if (Number.isNaN(Number(value))) {
     return def;
   }
-  return Number.parseInt(value, 10);
+  return +value;
 }
 
 /**
@@ -423,8 +423,8 @@ function toExponential(number, fractionDigits) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  return number.toFixed(fractionDigits);
 }
 
 /**
@@ -439,8 +439,8 @@ function toFixed(/* number, fractionDigits */) {
  * 12345, 7    => '12345.00'
  * 12.345, 4   => '12.35'
  */
-function toPrecision(/* number, precision */) {
-  throw new Error('Not implemented');
+function toPrecision(number, precision) {
+  return number.toPrecision(precision);
 }
 
 /**
@@ -453,8 +453,8 @@ function toPrecision(/* number, precision */) {
  * new Number(5) => 5
  * Number(-5)    => -5
  */
-function getNumberValue(/* number */) {
-  throw new Error('Not implemented');
+function getNumberValue(number) {
+  return number.valueOf();
 }
 
 /**
@@ -487,8 +487,8 @@ function isNumber(/* number */) {
  * 5.1  => false
  * '5'  => false
  */
-function isInteger(/* number */) {
-  throw new Error('Not implemented');
+function isInteger(number) {
+  return Number.isInteger(number);
 }
 
 /**
